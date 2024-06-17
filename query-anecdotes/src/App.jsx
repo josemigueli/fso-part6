580bf5995b0f7df4c493ade2de1edb8d340c8509
josemigueli <miguelusx@gmail.com>
2024-06-17 12:23:05 -0600

    Exercise 6.22

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,11 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './request'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './request'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (votedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      const newList = anecdotes.map(a => a.id !== votedAnecdote.id ? a : votedAnecdote)
+      queryClient.setQueryData(['anecdotes'], newList)
+    }
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    const setVote = {...anecdote, votes: anecdote.votes + 1}
+    voteAnecdoteMutation.mutate(setVote)
   }
 
   const result = useQuery({
@@ -14,8 +26,6 @@ const App = () => {
     retry: 1,
     refetchOnWindowFocus: false
   })
-
-  console.log(JSON.parse(JSON.stringify(result)))
 
   if (result.isLoading) {
     return <div>Loading data...</div>
--- query-anecdotes/src/request.js
+++ query-anecdotes/src/request.js
@@ -7,3 +7,6 @@ export const getAnecdotes = () =>
 
 export const createAnecdote =  anecdote =>
     axios.post(baseUrl, anecdote).then(res => res.data)
+
+export const updateAnecdote = updatedAnecdote =>
+    axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
